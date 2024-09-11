b1d6a10649c92979353a765d224a32021b2ddcf6
margarita2104 <margarita.p.ferreira@gmail.com>
2024-09-11 14:50:35 +0200

    test

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,9 +1,8 @@
 import Header from "../Shared/Header.jsx";
 import Hero from "../Shared/Hero.jsx";
 import { Link } from "react-router-dom";
-import Speaker from "../../assets/home-decor-sp.png"
-import Organizer from "../../assets/home-decor-org.png"
-
+import Speaker from "../../assets/home-decor-sp.png";
+import Organizer from "../../assets/home-decor-org.png";
 
 const HomePage = () => {
   return (
@@ -14,7 +13,8 @@ const HomePage = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-7 pl-20 pr-28">
           <div className="mr-14">
             <p className="mb-8 text-xl">
-              HI!!!!! More diversity on stages. Because you can not be what you can not see.
+              More diversity on stages. Because you can not be what you can not
+              see.
             </p>
             <p className="text-xl">
               With <strong>panelista</strong>, our aim is to provide access to a
@@ -23,7 +23,11 @@ const HomePage = () => {
             </p>
           </div>
           <div className="bg-lighter relative pt-40 pl-14 pr-14 pb-7 rounded-3xl shadow-lg text-center min-h-96 max-w-md flex flex-col justify-start">
-            <img className="absolute -top-36 -right-5" src={Speaker} alt="Speaker" />
+            <img
+              className="absolute -top-36 -right-5"
+              src={Speaker}
+              alt="Speaker"
+            />
             <p className="mb-auto text-2xl font-bold text-left ">
               We enable speakers to take the stage on their own terms.
             </p>
@@ -32,7 +36,11 @@ const HomePage = () => {
             </Link>
           </div>
           <div className="bg-lighter relative pt-40 pl-14 pr-14 pb-7 rounded-3xl shadow-lg text-center min-h-96 max-w-md flex flex-col justify-end">
-            <img className="absolute -top-32 -right-5" src={Organizer} alt="Organizer" />
+            <img
+              className="absolute -top-32 -right-5"
+              src={Organizer}
+              alt="Organizer"
+            />
             <p className="mb-auto text-2xl font-bold text-left ">
               We connect event organizers with diverse speakers and panelists.
             </p>
